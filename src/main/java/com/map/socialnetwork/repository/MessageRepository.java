0c883bcf81f9e7d10fe96ad0aae513aa58d04386
mihai.popescu <mihai.popescu@tora.com>
2021-12-27 19:21:47 +0200

    Get ID from DB when saving a message

--- src/main/java/com/map/socialnetwork/repository/MessageRepository.java
+++ src/main/java/com/map/socialnetwork/repository/MessageRepository.java
@@ -18,7 +18,7 @@ public class MessageRepository extends AbstractRepository{
         String saveReceiver = "insert into users_messages (user_id, message_id) values (?, ?)";
 
         try (Connection connection = DriverManager.getConnection(url, username, password);
-             PreparedStatement ps1 = connection.prepareStatement(saveMessage);
+             PreparedStatement ps1 = connection.prepareStatement(saveMessage, Statement.RETURN_GENERATED_KEYS);
              PreparedStatement ps2 = connection.prepareStatement(saveReceiver)) {
 
             ps1.setLong(1, message.getFrom().getId());
@@ -27,8 +27,13 @@ public class MessageRepository extends AbstractRepository{
             ps1.setTimestamp(4, message.getTime());
             ps1.executeUpdate();
 
-            ///TODO extract id from db
-            ps2.setLong(2, message.getId());
+            try (ResultSet generatedKeys = ps1.getGeneratedKeys()) {
+                if(generatedKeys.next()) {
+                    ps2.setLong(2, generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("Creation of message failed. Could not obtain the ID!");
+                }
+            }
 
             for (User user : message.getTo()) {
                 ps2.setLong(1, user.getId());
