ffead3e50d4e1eaccbaa88c30c5acea22fe4a86f
mihai.popescu <mihai.popescu@tora.com>
2021-12-29 20:21:22 +0200

    Observer pattern

--- src/main/java/com/map/socialnetwork/controllers/UserController.java
+++ src/main/java/com/map/socialnetwork/controllers/UserController.java
@@ -1,8 +1,6 @@
 package com.map.socialnetwork.controllers;
 
 import com.map.socialnetwork.Main;
-import com.map.socialnetwork.domain.Credentials;
-import com.map.socialnetwork.domain.Friendship;
 import com.map.socialnetwork.domain.User;
 import com.map.socialnetwork.exceptions.AuthenticationException;
 import com.map.socialnetwork.exceptions.MissingEntityException;
@@ -13,17 +11,21 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 
-public class UserController {
+public class UserController implements Observer {
     private Authentication authentication;
     private Service service;
     private Stage primaryStage;
@@ -67,9 +69,9 @@ public class UserController {
 
     @FXML
     public void initialize() {
-        FirstNameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("firstName"));
-        LastNameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("lastName"));
-        IdColumn.setCellValueFactory(new PropertyValueFactory<User, Long>("id"));
+        FirstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        LastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        IdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
 
         friendsTable.setItems(model);
     }
@@ -80,7 +82,6 @@ public class UserController {
         List<User> friendsList = StreamSupport.stream(friends.spliterator(), false)
                 .collect(Collectors.toList());
         model.setAll(friendsList);
-
     }
 
     @FXML
@@ -99,8 +100,9 @@ public class UserController {
 
     @FXML
     public void handleRemoveFriend() throws MissingEntityException {
-        User user = friendsTable.getSelectionModel().getSelectedItem();
-        service.setFriendshipStatus(userId, user.getId(), Friendship.Status.REJECTED);
+        long id1 = authentication.getUserId();
+        long id2 = friendsTable.getSelectionModel().getSelectedItem().getId();
+        service.removeFriendship(id1, id2);
         initModel();
     }
 
@@ -124,4 +126,8 @@ public class UserController {
         loginController.setStage(primaryStage);
     }
 
+    @Override
+    public void update(Observable o, Object arg) {
+        initModel();
+    }
 }
